0fc7389de589bdca2029fa64e84cf0f792b8c91e
AnnasophiaSanchez-Acosta <annasophia.sanchez-acosta@stu.ocsb.ca>
2025-06-11 00:41:27 +0000

    Done hopefully?

--- js/script.js
+++ js/script.js
@@ -3,3 +3,25 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+'use strict'
+
+/**
+ * This function gets a random fun fact from the API
+ * The 'async' keyword is used because it will take time from the internet to get data
+ */
+async function getCatImage() {
+  try {
+    const response = await fetch(
+      'https://catfact.ninja/fact'
+    )
+    const data = await response.json()
+    console.log(data)
+
+    // Output the fun fact to the page
+    document.getElementById('show-cat').innerHTML =
+      '<b>Cat fun fact:</b> "' + data.fact + '"<br />'
+  } catch (error) {
+    console.error(error)
+  }
+}
